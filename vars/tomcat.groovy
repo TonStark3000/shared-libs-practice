ef1b6c23f8ad994255f79f626e4ed6fd286e606d
TonStark3000 <138349085+TonStark3000@users.noreply.github.com>
2024-04-24 20:49:45 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -1,5 +1,5 @@
 def call(Credentials, ip){
-   sshagent("$Credentials") {
+   sshagent("${Credentials}") {
                         sh "scp -o StrictHostKeyChecking=no target/myweb.war  ec2-user@${ip}:/opt/tomcat-9/webapps/" 
                         
    }
